f846813fc63a6897f9f81abf321061ab6a9e527c
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-21 20:06:38 +0330

    working on model

---
+++ djangoapi/courses/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    language = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
